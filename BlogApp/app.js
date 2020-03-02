bac6dcd0bf520d90d71377617ed9b99f009f9bdb
nemanjakontic <nemanja.kontic28@gmail.com>
2020-03-02 20:58:57 +0100

    gotov create i show

--- BlogApp/app.js
+++ BlogApp/app.js
@@ -39,6 +39,20 @@ app.get("/blogs", function(req, res) {
     });
 });
 
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+
+app.post("/blogs", function(req, res) {
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if (err) {
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
 //////////////////////////
 
 var port = process.env.PORT || 3000;
